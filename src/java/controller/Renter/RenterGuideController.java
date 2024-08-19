885f6996e502b58ada3cb73e2a57e934ecf153b7
He163623 <Datpthe163623@fpt.edu.vn>
2024-08-19 10:58:06 +0700

    ss

---
+++ src/java/controller/Renter/RenterGuideController.java
@@ -0,0 +1,40 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package controller.Renter;
+
+import controller.Owner.*;
+import com.google.gson.Gson;
+import dao.GuideLineDAO;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.util.ArrayList;
+import model.Guideline;
+
+/**
+ *
+ * @author DAT
+ */
+@WebServlet(name = "RenterGuideController", urlPatterns = {"/renterguideline"})
+public class RenterGuideController extends HttpServlet {
+    
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        GuideLineDAO dbGuideline = new GuideLineDAO();
+        ArrayList<Guideline> guideLines = dbGuideline.findAll();
+        
+        req.setAttribute("guidelines", guideLines);
+        
+        req.getRequestDispatcher("Renter/guidelinelist.jsp").forward(req, resp);
+    }
+    
+    @Override
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+    }
+    
+}
